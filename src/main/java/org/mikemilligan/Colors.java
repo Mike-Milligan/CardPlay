10ea8df69085f9995edb291958628b2bc946b5c0
Mike Milligan <mimilli022@student.wethinkcode.co.za>
2024-01-17 15:25:50 +0200

    Made all colors public

--- src/main/java/org/mikemilligan/Colors.java
+++ src/main/java/org/mikemilligan/Colors.java
@@ -1,10 +1,10 @@
 package org.mikemilligan;
 
 public class Colors {
-	
+
 	public static final String CYAN = "\u001B[36m"; //Prompt text
-	static final String RESET = "\u001B[0m"; // Reset text to default
-	static final String RED = "\u001B[31m"; //Red error text
-	static final String GREEN = "\u001B[32m"; //Green success text
-	static final String PURPLE = "\u001B[35m"; //User input text
+	public static final String RESET = "\u001B[0m"; // Reset text to default
+	public static final String RED = "\u001B[31m"; //Red error text
+	public static final String GREEN = "\u001B[32m"; //Green success text
+	public static final String PURPLE = "\u001B[35m"; //User input text
 }
